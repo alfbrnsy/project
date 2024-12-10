7e4160e8c7826d8d1f033b3579f66a20f4a097e6
alfbrnsy <aldofebriansyah0224@gmail.com>
2024-12-10 20:43:00 +0700

    test push, aldo

--- config/database.php
+++ config/database.php
@@ -1,11 +1,7 @@
 <?php
 
 // Konfigurasi koneksi database
-<<<<<<< HEAD
-$serverName = "HUSEIN"; // Nama server
-=======
 $serverName = "VICTUS\DBMSALDO"; // Nama server
->>>>>>> 17a502dfefb12574709b37092a73c7ab30663d3f
 $connectionInfo = array("Database" => "si_tata_tertib"); // Nama database
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
